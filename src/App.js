56bdeec9bb840cd49ada973ddbf15dfde1c9963e
Arthur Kho <islandjoe@gmail.com>
2019-02-01 22:45:42 +0200

    Setup Sidebar and Map

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import {Sidebar, Tab} from 'react-leaflet-sidetabs'
 import { FiHome, FiChevronRight, FiSearch, FiSettings } from 'react-icons/fi'
 // import SearchPanel from './control/SearchPanel'
 
+import 'leaflet/dist/leaflet.css'
 import './App.css'
 
 const tonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png'
 const tonerAttrb = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-const mapCenter = [39.9528, -75.1638]
+const mapCenter = [60.1713, 24.8280]
 const zoomLevel = 12
 
 class App extends Component {
@@ -35,28 +36,30 @@ class App extends Component {
       <div className="App">
         <Sidebar
           id="sidebar"
-          position="right"
-          collapsed={this.state.collapsed}
-          closeIcon={<FiChevronRight />}
-          selected={this.state.selected}
-          onOpen={this.onOpen.bind(this)}
-          onClose={this.onClose.bind(this)}
+            position="right"
+              collapsed={this.state.collapsed}
+                closeIcon={<FiChevronRight />}
+                  selected={this.state.selected}
+                    onOpen={this.onOpen.bind(this)}
+                      onClose={this.onClose.bind(this)}
         >
            <Tab id="home" header="Home" icon={<FiHome />}>
             <p>No place like home!</p>
            </Tab>
+
            <Tab id="search" header="Search" icon={<FiSearch />}>
             <p>The noblest search is the search for excellence!</p>
            </Tab>
+
            <Tab id="settings" header="Settings" anchor="bottom" icon={<FiSettings />}>
             <p>We don't want privacy so much as privacy settings!</p>
            </Tab>
         </Sidebar>
 
-        <Map className="mapStyle" center={[0, 0]} zoom={7}>
+        <Map className="mapStyle" center={mapCenter} zoom={zoomLevel}>
           <TileLayer
-            attribution=""
-            url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
+            attribution={tonerAttrb}
+            url={tonerTiles}
           />
         </Map>
       </div>
