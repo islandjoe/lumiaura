141c51b57a19522e0dddbb676d08fac539bc2bca
Arthur Kho <islandjoe@gmail.com>
2019-02-14 21:01:57 +0200

    Adjust URL to use https in Github

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ const plowMarker = L.icon({
   iconSize: [20, 20]
 })
 
-const tonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png'
-const tonerAttrb = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+const tonerTiles = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png'
+const tonerAttrb = 'Map tiles by <a href="https://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 const mapCenter = [60.1713, 24.8280]
 const zoomLevel = 12
 
